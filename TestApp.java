8996e7d163f2ad92f6b9aa56802e8644ec5eff24
= <hamidjaffari21@gmail.com>
2026-04-19 11:57:16 +0430

    add student 2

--- TestApp.java
+++ TestApp.java
@@ -2,5 +2,8 @@ public class TestApp {
     public static void main(String[] args) {
         Student s1 = new Student("Ahmad", "Nasiri", 001,"Barchi-tank til");
         s1.show();
+
+        Student s2 = new Student("Altaf", "Ibrahimi", 002, "Tanktil Onchi");
+        s2.show();
     }
 }

148cd991184f84d9a9760c9084c05f28cc2260f9
= <hamidjaffari21@gmail.com>
2026-04-19 11:53:16 +0430

    add student address

--- Student.java
+++ Student.java
@@ -2,16 +2,19 @@ public class Student {
     String name;
     String lastName;
     int id;
+    String address;
 
-    public Student(String name, String lastName , int id){
+    public Student(String name, String lastName , int id, String address){
         this.name = name;
         this.lastName = lastName;
         this.id = id;
+        this.address = address;
     }
 
     public void show(){
         System.out.println("Name :"+name);
         System.out.println("Lastname :"+lastName);
         System.out.println("ID :"+id);
+        System.out.println("Address :"+address);
     }
 }
--- TestApp.java
+++ TestApp.java
@@ -1,6 +1,6 @@
 public class TestApp {
     public static void main(String[] args) {
-        Student s1 = new Student("Ahmad", "Nasiri", 001);
+        Student s1 = new Student("Ahmad", "Nasiri", 001,"Barchi-tank til");
         s1.show();
     }
 }
